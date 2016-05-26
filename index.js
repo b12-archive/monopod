1ce8e48909e87c01065401c62fe37bb7bfee3053
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2016-05-26 18:29:35 +0200

    Basic module structure

---
+++ index.js
@@ -0,0 +1,3 @@
+const bootstrap = require('./bootstrap');
+
+module.exports = { bootstrap };
--- test.js
+++ test.js
@@ -2,3 +2,13 @@ require('tap-spec-integrated');
 const test = require('tape-catch');
 
 const monopod = require('.');
+const bootstrap = require('./bootstrap');
+
+test('monopod exports submodules as properties', (is) => {
+  is.equal(
+    monopod.bootstrap,
+    bootstrap
+  );
+
+  is.end();
+});
