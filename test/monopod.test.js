9082b6a3474adc0c56875a17e840e4d8b744d862
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2016-06-11 15:34:40 +0200

    Fix exports test

--- test/monopod.test.js
+++ test/monopod.test.js
@@ -3,11 +3,23 @@ const test = require('tape-catch');
 
 const monopod = require('..');
 const bootstrap = require('../bootstrap');
+const debootstrap = require('../debootstrap');
+const release = require('../release');
 
 test('monopod exports submodules as properties', (is) => {
   is.equal(
     monopod.bootstrap,
     bootstrap
+  );
+
+  is.equal(
+    monopod.debootstrap,
+    debootstrap
+  );
+
+  is.equal(
+    monopod.release,
+    release
   );
 
   is.end();
